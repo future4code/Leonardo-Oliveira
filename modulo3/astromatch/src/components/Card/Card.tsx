93f06a260254f19c27342a1fe3deb106111aa9aa
leozartino <leozartino@gmail.com>
2021-12-04 19:50:22 -0300

    feat: add card e assets de exemplo

--- modulo3/astromatch/src/components/Card/Card.tsx
+++ modulo3/astromatch/src/components/Card/Card.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Actions from '../Actions/Actions';
+import { CardContainer } from './Card.styled';
 
 export const Card: React.FC = () => {
   return (
-    <div>
-      <h2>sss</h2>
-    </div>
+    <CardContainer>
+      <div className="photo">
+        <div className="text-user">
+          <div className="name-age">
+            <h2>Nome usuáiro</h2>
+            <h2>idade usuário</h2>
+          </div>
+          <div className="bio">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            iste veniam aliquid doloribus sapiente eos, vitae quas soluta sint
+            molestias.
+          </div>
+        </div>
+      </div>
+      <Actions />
+    </CardContainer>
   );
 };
