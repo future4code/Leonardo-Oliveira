09816aec118d1c487b9f4fc8b1389996ede2bfe3
leozartino <leozartino@gmail.com>
2021-12-13 20:49:43 -0300

    refactor: change routes for a file

--- modulo3/projeto-labex/src/App.tsx
+++ modulo3/projeto-labex/src/App.tsx
@@ -1,31 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import DataProvider from "./context/Data";
-import AdminPage from "./pages/AdminPage/AdminPage";
-import Home from "./pages/Home/Home";
-import ViagemPage from "./pages/ViagemPage/ViagemPage";
+import RoutesComponent from "./pages/routes";
 
 const App = function (): JSX.Element {
-  return (
-    <DataProvider>
-      <div className="app">
-        <BrowserRouter>
-          <Header />
-
-          <Routes>
-            <Route path="/" element={<Home />} />
-
-            <Route path="/viagem" element={<ViagemPage />} />
-
-            <Route path="/admin" element={<AdminPage />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-      <Footer />
-    </DataProvider>
-  );
+  return <RoutesComponent />;
 };
 
 export default App;
