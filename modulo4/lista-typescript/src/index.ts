b5bad2fb1b7c1e5b41cad18b73101ad86af5f3fa
Leonardo Oliveira <leozartino@gmail.com>
2022-01-23 14:48:46 -0300

    feat: add exercicio5

--- modulo4/lista-typescript/src/index.ts
+++ modulo4/lista-typescript/src/index.ts
@@ -2,6 +2,7 @@ import ExibeMensagemFormatada from "./exercicio1/exercicio1";
 import ExibeTipo from "./exercicio2/exercicio2";
 import { ExibeFilme, GENERO } from "./exercicio3/exercicio3";
 import { colaboradores, ExibeColaboradorSetor, SETOR } from "./exercicio4/exercicio4";
+import { ExibeUsuarioRole, usuarios } from "./exercicio5/exercicio5";
 
 
 console.log(ExibeMensagemFormatada("Leonardo", "15/10/1996"));
@@ -10,7 +11,8 @@ console.log(ExibeTipo(5));
 
 
 console.log(ExibeFilme("The shining", 1980, GENERO.TERROR));
-
 console.log(ExibeFilme("The shining", 1980, GENERO.TERROR, 66));
 
 console.log(ExibeColaboradorSetor(colaboradores, SETOR.MARKETING));
+
+console.log(ExibeUsuarioRole(usuarios, "admin"));
