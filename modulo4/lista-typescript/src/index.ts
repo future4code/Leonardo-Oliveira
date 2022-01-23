160eca76ac1e970f336bc3e42ffce9234d3cf0a7
Leonardo Oliveira <leozartino@gmail.com>
2022-01-23 14:00:52 -0300

    feat: add exercicio2

--- modulo4/lista-typescript/src/index.ts
+++ modulo4/lista-typescript/src/index.ts
@@ -1,3 +1,7 @@
 import ExibeMensagemFormatada from "./exercicio1/exercicio1";
+import ExibeTipo from "./exercicio2/exercicio2";
+
 
 console.log(ExibeMensagemFormatada("Leonardo", "15/10/1996"));
+
+console.log(ExibeTipo(5));
