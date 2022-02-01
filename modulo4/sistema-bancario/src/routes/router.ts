cb8c9e483e7ccdf81260cbd4176cd1a6315bf4e7
Leonardo Oliveira <leozartino@gmail.com>
2022-01-31 22:45:38 -0300

    style: minor change style code

--- modulo4/sistema-bancario/src/routes/router.ts
+++ modulo4/sistema-bancario/src/routes/router.ts
@@ -8,7 +8,6 @@ router.get('/users', userController.indexUsers);
 router.get('/users/balance', userController.showBalance);
 router.post('/users', userController.createUser);
 router.put('/users/balance', userController.addBalnce);
-router.put('/users/bill/:cpf', userController.payBill
-);
+router.put('/users/bill/:cpf', userController.payBill);
 
 export default router;
